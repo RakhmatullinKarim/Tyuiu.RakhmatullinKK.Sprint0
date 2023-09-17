91f4341a39a73a872eaecbb43bc6d353d0eb68c1
Tyuiu.RakhmatullinKK@outlook.com <Tyuiu.RakhmatullinKK@outlook.com>
2023-09-17 21:34:07 +0500

    Task2

---
+++ Tyuiu.RakhmatullinKK.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.RakhmatullinKK.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.RakhmatullinKK.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+
+            var name = "Karim";
+            var res = DataService.GetMessage(name);
+
+
+            Assert.AreEqual("Privet, Karim", res);
+        }
+    }
+}
---
+++ Tyuiu.RakhmatullinKK.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Tyuiu.RakhmatullinKK.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.RakhmatullinKK.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(DataService.GetMessage("Karim"));
+            Console.ReadKey();
+        }
+    }
+}
